098a8b5ebf02465d67331f77ff0d73d8924fd0d9
Paulina <Paulina@DESKTOP-AOIST93>
2023-04-19 14:08:08 +0200

    Add project files.

---
+++ 20230303AlgorytmySortujaceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("20230303AlgorytmySortujaceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20230303AlgorytmySortujaceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("508ce3f4-704b-4c60-9415-2e08e051f18f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 20230303AlgorytmySortujaceTest/UnitTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace TestingSortingAlgorithms
+{
+    // Paulina Sikorska
+    [TestClass]
+    public class UnitTests
+    {
+        int[] tablica = { 73, 93, 151, 8, 166, 190, 164, 169, 185, 123, 12, 60, 13, 20, 64, 153, 15, 111, 187, 193, 79, 156, 14, 45, 168, 197, 133, 186, 103, 102, 71, 179, 163, 27, 199, 43, 159, 100, 26, 9, 75, 150, 29, 37, 181, 106, 72, 61, 57, 56 };
+        int[] expected = { 8, 9, 12, 13, 14, 15, 20, 26, 27, 29, 37, 43, 45, 56, 57, 60, 61, 64, 71, 72, 73, 75, 79, 93, 100, 102, 103, 106, 111, 123, 133, 150, 151, 153, 156, 159, 163, 164, 166, 168, 169, 179, 181, 185, 186, 187, 190, 193, 197, 199 };
+        int[] actual;
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            actual = SortingAlgorithms.SelectionSort(tablica);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+        [TestMethod]
+        public void TestSelectionSort2()
+        {
+            actual = SortingAlgorithms.SelectionSort(tablica);
+            CollectionAssert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void TestInsertionSort()
+        {
+            SortingAlgorithms.InsertionSort(tablica);
+            CollectionAssert.AreEqual(tablica, expected);
+        }
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            SortingAlgorithms.QuickSort(tablica, 0,49);
+            CollectionAssert.AreEqual(tablica, expected);
+        }
+    }
+}
